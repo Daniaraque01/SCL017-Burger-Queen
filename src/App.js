685a5148368b6387535701a5e5fb270f2ad7990f
NicolePozo <nicolepozo.salas@gmail.com>
2021-09-23 16:56:49 -0400

    boostrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import SeleccionaTuRol from "./components/SeleccionaTuRol.jsx"
+import SeleccionaTuRol from "./components/SeleccionaTuRol"
 import Cocina from './components/Cocina';
 import TomarPedido from './components/TomarPedido';
 import SushiSanKun from './assets/SushiSanKun.png';
+import imagenMesero from './assets/imagenMesero.png'
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 import {
   BrowserRouter as Router,
@@ -22,6 +25,14 @@ function App() {
       <Switch>
       <Route path="/" exact>
         <SeleccionaTuRol/>
+        <div className="container">
+          <div className="row">
+            <div className="col-3"></div>
+            <img src={imagenMesero} alt="" />
+          <button type="button" className="btn btn-primary m-12">hola</button>
+          <button type="button" className="btn btn-primary">hola</button>
+          </div>
+      </div>
     </Route>
 
     <Route path="/cocina" exact>
