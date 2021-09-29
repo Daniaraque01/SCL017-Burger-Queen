dd643099cf4bba32cd7a8503239e61ef7f5336e2
Daniela Araque <daniaraque05@gmail.com>
2021-09-29 09:30:21 -0300

    haciendo contador

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,15 +4,19 @@ import "bootstrap/dist/css/bootstrap.css";
 import Titulo from './Titulo';
 import './Card.css';
 import TomarPedido from './TomarPedido';
+import {useState} from 'react';
 
         const Card = () => {
             const loadData = () =>JSON.parse(JSON.stringify(menu));
         
-            return (
+const [number, setNumber] = useState(0);
+const [numberLess,setNumberLess] = useState(setNumber);
         
-                <div className="card mb-3 cardShadow pantallaDos">
+            return (
+                <div className="pantallaDos">
                     <TomarPedido />
                     <Titulo />
+                    <div className="card mb-3 cardShadow">
           <div className="row no-gutters">
             <div className="col-md-3">
               <img src="https://upload.wikimedia.org/wikipedia/commons/7/74/Tesla_the_cat_2.jpg" class="card-img" alt=""/>
@@ -23,12 +27,13 @@ import TomarPedido from './TomarPedido';
                 {menu.map((data, index)=>{
                     return <p key={index} className="card-text">{data.tablas.palta.descripcion}</p>
                 })}
-                <button className="btn btn-warning btn-sm">-</button>
-                <button className="btn btn-warning btn-md">0</button>
-                <button className="btn btn-warning btn-sm">+</button>
+                <button onClick={() => setNumberLess(numberLess - 1)} className="btn btn-warning btn-sm">-</button>
+                <h3>{number}</h3>
+                <button onClick={() => setNumber(number + 1)} className="btn btn-warning btn-sm">+</button>
                 <h3 className="float-h3">PRECIO</h3>
               </div>
             </div>
+          </div>
           </div>
         </div>
             )
