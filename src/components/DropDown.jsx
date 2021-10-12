673e1b9bb4408ab3117bc224648ff38e610def46
NicolePozo <nicolepozo.salas@gmail.com>
2021-10-12 16:11:40 -0400

    se muestran las comandas

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 import { Fragment } from 'react'
 
 const DropDown = () => {
+
+/* const getSelectedValue = () =>{
+ document.getElementById("dropDownMesas").value; 
+ console.log(getSelectedValue)
+} */
+
+
+  
     return (
         <div>
-            <div class="dropdown">
-  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"> {/* deberia ser un select */}
+            <div class="dropdown" id="dropDownMesas" /* onChange={getSelectedValue} */>
+  <button type="button" class="btn btn-primary dropdown-toggle"  data-toggle="dropdown"> {/* deberia ser un select */}
     Mesa
   </button>
   <div class="dropdown-menu">
-    <a class="dropdown-item" href="#">Mesa 1</a>
-    <a class="dropdown-item" href="#">Mesa 2</a>
-    <a class="dropdown-item" href="#">Mesa 3</a>
+    <a class="dropdown-item" value= "mesa1" href="#">Mesa 1</a>
+    <a class="dropdown-item" value = "mesa2" href="#">Mesa 2</a>
+    <a class="dropdown-item" value= "mesa3" href="#">Mesa 3</a>
   </div>
 </div>
             
